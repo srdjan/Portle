5584cb49aa896177b6b274b40d2b15a2032dac12
Timur Badretdinov <destinerx@gmail.com>
2019-10-28 16:58:18 +0300

    Add Login page to router

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import VueRouter from 'vue-router';
 
 import App from './App.vue';
 
+import Login from './pages/Login.vue';
 import Main from './pages/Main.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
+	{ path: '/login', component: Login },
 	{ path: '/', component: Main }
 ];
 
