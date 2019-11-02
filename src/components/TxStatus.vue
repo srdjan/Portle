13fb36594428c5fb0c361a60da2f054b9290c456
Timur Badretdinov <destinerx@gmail.com>
2019-11-02 16:29:28 +0300

    Add TxStatus component

---
+++ src/components/TxStatus.vue
@@ -0,0 +1,59 @@
+<template>
+	<div id="txStatus" :class="{'shown': status != 'none'}">
+		{{ text }}
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['status', 'onHidden'],
+	data() {
+		return {
+			lastStatus: '',
+			timeoutId: 0,
+		}
+	},
+	watch: {
+		status(value) {
+			clearTimeout(this.timeoutId);
+			if (this.status == 'mining') {
+				return;
+			}
+			if (this.status != 'none') {
+				this.lastStatus = this.status;
+			}
+			this.timeoutId = setTimeout(this.onHidden, 10 * 1000);
+		},
+	},
+	computed: {
+		text() {
+			const textMap = {
+				'rejected': 'Transaction rejected.',
+				'mining': 'Mining the transaction…',
+				'success': 'Transaction complete.',
+				'failure': 'Transaction failed.',
+			};
+			const text = textMap[this.status] || textMap[this.lastStatus];
+			return text;
+		},
+	},
+}
+</script>
+
+<style scoped>
+#txStatus {
+	position: absolute;
+	bottom: 0px;
+	right: 32px;
+	font-size: 0.8em;
+	border: 1px solid black;
+	padding: 0.5em 1.5em;
+	opacity: 0;
+	transition: bottom 1s ease, opacity 1s ease;
+}
+
+#txStatus.shown {
+	bottom: 32px;
+	opacity: 1;
+}
+</style>
