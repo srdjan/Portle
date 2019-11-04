2b3ac24fb24e0f4e182b659dbafcd03854dc4a07
Timur Badretdinov <destinerx@gmail.com>
2019-11-04 15:03:57 +0300

    Add sendTx mixin

---
+++ src/mixins/sendTx.js
@@ -0,0 +1,23 @@
+export const sendTx = {
+	data() {
+		return {
+			txStatus: 'none',
+		}
+	},
+	methods: {
+		async _sendTx(txPromise) {
+			try {
+				this.txStatus = 'mining';
+				const tx = await txPromise;
+				const txReceipt = await provider.getTransactionReceipt(tx.hash);
+				if (txReceipt.status == 1) {
+					this.txStatus = 'success';
+				} else {
+					this.txStatus = 'failure';
+				}
+			} catch(e) {
+				this.txStatus = 'rejected';
+			}
+		},
+	},
+}
