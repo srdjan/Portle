42afe76636bf30e1d1830260fbfa5932c5255dfb
Timur Badretdinov <destinerx@gmail.com>
2019-11-05 13:44:55 +0300

    Add AssetInput component

---
+++ src/components/AssetInput.vue
@@ -0,0 +1,51 @@
+<template>
+	<span class="input-group">
+		<span
+			class="max-label"
+			@click="setMax()"
+		>
+			MAX
+		</span>
+		<input
+			:value="amount"
+			@input="onAmountChange"
+			class="amount"
+		>
+		<span class="label label-ghost label-right inline">{{ formatAsset(id) }}</span>
+	</span>
+</template>
+
+<script>
+import tickers from '../data/tickers.json';
+
+export default {
+	props: {
+		id: {
+			type: String,
+			default: 'dai',
+		},
+		amount: {
+			type: String,
+			default: '0',
+		},
+		setMax: {
+			type: Function,
+			default: () => {},
+		},
+		onChange: {
+			type: Function,
+			default: () => {},
+		},
+	},
+	methods: {
+		formatAsset(id) {
+			const ticker = tickers[id];
+			return ticker;
+		},
+		onAmountChange(event) {
+			const amount = event.target.value;
+			this.onChange(this.id, amount);
+		},
+	},
+};
+</script>
