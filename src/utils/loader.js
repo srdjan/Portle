001fbd26754254bfdfbe00531583bcc391fa737b
Timur Badretdinov <destinerx@gmail.com>
2019-11-07 12:39:42 +0300

    Add Converter.loadBalance

--- src/utils/loader.js
+++ src/utils/loader.js
@@ -1,3 +1,5 @@
+import Converter from './converter.js';
+
 import coinIds from '../data/coin-ids.json';
 
 class Loader {
@@ -9,6 +11,32 @@ class Loader {
 		const prices = await response.json();
 		const usdPrices = assetIds.map((assetId) => prices[assetId].usd);
 		return usdPrices; 
+	}
+
+	static async loadBalance(address) {
+		const url = `https://api.ethplorer.io/getAddressInfo/${address}?apiKey=freekey`;
+		const response = await fetch(url);
+		const balanceResponse = await response.json();
+		const balances = {};
+		balances['eth'] = {
+			balance: Converter.toBalance(balanceResponse.ETH.balance, 'eth'),
+		};
+		const tokens = balanceResponse.tokens || [];
+		for (const tokenData of balanceResponse.tokens) {
+			const assetId = tokenData.tokenInfo.symbol.toLowerCase();
+			if (assetId == '') {
+				continue;
+			}
+			const balance = tokenData.balance.toString();
+			const price = tokenData.tokenInfo.price
+				? tokenData.tokenInfo.price.rate
+				: undefined;
+			balances[assetId] = {
+				balance,
+				price,
+			};
+		}
+		return balances;
 	}
 }
 
