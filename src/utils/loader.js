58027d835e35b12a6085538ab517471923ab6927
Timur Badretdinov <destinerx@gmail.com>
2019-11-07 10:42:45 +0300

    Add Loader util class

---
+++ src/utils/loader.js
@@ -0,0 +1,15 @@
+import coinIds from '../data/coin-ids.json';
+
+class Loader {
+	static async loadPrices(assets) {
+		const assetIds = assets.map((asset) => coinIds[asset]);
+		const assetIdString = assetIds.join('%2C');
+		const url = `https://api.coingecko.com/api/v3/simple/price?ids=${assetIdString}&vs_currencies=usd`;
+		const response = await fetch(url);
+		const prices = await response.json();
+		const usdPrices = assetIds.map((assetId) => prices[assetId].usd);
+		return usdPrices;
+	}
+}
+
+export default Loader;
