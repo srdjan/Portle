27d61ce44eb9d012d6d31258b4c50d137cb61f01
Timur Badretdinov <destinerx@gmail.com>
2019-12-05 16:04:09 +0300

    Add formatSet to formatter

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -59,6 +59,30 @@ class Formatter {
 		};
 		return rate.toLocaleString(undefined, options);
 	}
+
+	static formatSet(setId) {
+		const sets = {
+			'btcdai': 'BTCHIVOL',
+			'btceth2575': 'BTCETH2575',
+			'btceth5050': 'BTCETH5050',
+			'btceth7525': 'BTCETH7525',
+			'btcminvol': 'BTCMINVOL',
+			'eth12emaco': 'ETH12EMACO',
+			'eth20smaco': 'ETH20SMACO',
+			'eth26emaco': 'ETH26EMACO',
+			'eth50smaco': 'ETH50SMACO',
+			'ethbtc26emaco': 'ETHBTCEMACO',
+			'ethdai': 'ETHHIVOL',
+			'ethminvol': 'ETHMINVOL',
+			'ethrsi6040': 'ETHRSI6040',
+			'ethbtcrsi7030': 'ETHBTCRSI7030',
+			'ieth20smaco': 'iETH20SMACO',
+			'ieth50smaco': 'iETH50SMACO',
+			'stbtcdai': 'BTCLOVOL',
+			'stethdai': 'ETHLOVOL',
+		};
+		return sets[setId];
+	}
 }
 
 export default Formatter;
