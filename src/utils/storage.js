6ae98371c4057c076b16a4e965cb81817e7ef3f9
Timur Badretdinov <destinerx@gmail.com>
2020-01-14 19:42:13 +0300

    Add Storage util

---
+++ src/utils/storage.js
@@ -0,0 +1,30 @@
+const ACCOUNT_KEY = 'accounts';
+
+class Storage {
+	static getWalletList() {
+		const listString = localStorage.getItem(ACCOUNT_KEY) || '[]';
+		const list = JSON.parse(listString);
+		return list;
+	}
+
+	static addWallet(wallet) {
+		this.removeWallet(wallet);
+		const walletList = this.getWalletList();
+		walletList.push(wallet);
+		_saveWalletList(walletList);
+	}
+
+	static removeWallet(wallet) {
+		const walletList = this.getWalletList();
+		const walletIndex = walletList.findIndex(w => w.address == wallet.address);
+		walletList.splice(walletIndex, 1);
+		_saveWalletList(walletList);
+	}
+}
+
+function _saveWalletList(walletList) {
+	const listString = JSON.stringify(walletList);
+	localStorage.setItem(ACCOUNT_KEY, listString);
+}
+
+export default Storage;
