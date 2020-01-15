e8283a6ff05ff4059be3250813541236ba1a3bc2
Timur Badretdinov <destinerx@gmail.com>
2020-01-16 02:26:20 +0300

    Add WalletIcon component

---
+++ src/components/WalletIcon.vue
@@ -0,0 +1,31 @@
+<template>
+	<div
+		:class="{ 
+			'id-0': id == 0,
+		}"
+	/>
+</template>
+
+<script>
+export default {
+	props: {
+		id: {
+			type: Number,
+			default: 0,
+		},
+	},
+};
+</script>
+
+<style scoped>
+div {
+	height: 2em;
+	width: 2em;
+	border-radius: 1em;
+	background: #d6d6d6;
+}
+
+.id-0 {
+	background: teal;
+}
+</style>
