34baaf1d4779fc7de188c459891a7c3a8ff25771
Timur Badretdinov <destinerx@gmail.com>
2020-01-16 15:04:38 +0300

    Use WalletList in Portfolio

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,46 +1,11 @@
 <template>
 	<div id="view">
-		<section id="wallet-section">
-			<div>
-				<div id="total-value">
-					Net worth: {{ formatMoney(totalBalance) }}
-				</div>
-
-				<div id="wallet-list">
-					<div id="wallet-list-header">
-						Wallets
-					</div>
-					<div
-						v-for="(wallet, walletId) in wallets"
-						:key="wallet.address"
-						class="wallet"
-					>
-						<WalletIcon :id="walletId" />
-						<div class="wallet-details">
-							<div class="wallet-address">
-								{{ formatWalletAddress(wallet.address) }}
-							</div>
-							<div class="wallet-value">
-								{{ formatMoney(walletBalance(walletId)) }}
-							</div>
-						</div>
-					</div>
-					<div id="add-wallet">
-						<div id="add-wallet-icon">
-							<img :src="plusIcon">
-						</div>
-						<div id="add-wallet-text">
-							<div id="add-wallet-title">
-								Add new wallet
-							</div>
-							<div id="add-wallet-subtitle">
-								Metamask, ENS, address, etc
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		<WalletList
+			id="wallet-section"
+			:wallets="wallets"
+			:prices="prices"
+			:components="components"
+		/>
 		<div id="asset-section">
 			<div
 				v-if="assets.length > 0"
@@ -112,19 +77,17 @@ import Storage from '../utils/storage.js';
 
 import addresses from '../data/addresses.json';
 
-import plusIcon from '../../public/img/plus.svg';
-
 import AssetList from '../components/group/AssetList.vue';
 import DepositList from '../components/group/DepositList.vue';
 import InvestmentList from '../components/group/InvestmentList.vue';
-import WalletIcon from '../components/WalletIcon.vue';
+import WalletList from '../components/group/WalletList.vue';
 
 export default {
 	components: {
 		AssetList,
 		DepositList,
 		InvestmentList,
-		WalletIcon,
+		WalletList,
 	},
 	data() {
 		return {
@@ -226,19 +189,6 @@ export default {
 			}
 			return investments;
 		},
-		totalBalance() {
-			if (this.wallets.length == 0) {
-				return '0';
-			}
-			const balance = Balance.getTotal(
-				this.assets,
-				this.deposits,
-				this.investments,
-				this.components,
-				this.prices
-			);
-			return balance.toString();
-		},
 		assetBalance() {
 			const balance = Balance.getAssets(this.assets, this.prices);
 			return balance.toString();
@@ -250,9 +200,6 @@ export default {
 		investmentBalance() {
 			const balance = Balance.getInvestments(this.investments, this.components, this.prices);
 			return balance.toString();
-		},
-		plusIcon() {
-			return plusIcon;
 		},
 	},
 	async mounted() {
@@ -266,27 +213,8 @@ export default {
 		this._loadPrices();
 	},
 	methods: {
-		walletBalance(walletId) {
-			const walletAssets = this.assets
-				.filter(asset => asset.walletId == walletId);
-			const walletDeposits = this.deposits
-				.filter(deposit => deposit.walletId == walletId);
-			const walletInvestments = this.investments
-				.filter(investment => investment.walletId == walletId);
-			const walletBalance = Balance.getTotal(
-				walletAssets,
-				walletDeposits,
-				walletInvestments,
-				this.components,
-				this.prices
-			);
-			return walletBalance.toString();
-		},
 		formatMoney(moneyString) {
 			return Formatter.formatMoney(moneyString);
-		},
-		formatWalletAddress(address) {
-			return Formatter.formatAddress(address);
 		},
 		_initWallets(walletList) {
 			this.wallets = [];
@@ -630,64 +558,6 @@ export default {
 	justify-content: space-between;
 	flex: 1;
 	padding: 1.5em 1em;
-}
-
-#total-value {
-	font-size: 1.125em;
-	font-weight: bold;
-}
-
-#wallet-list {
-	margin-top: 2.25em;
-}
-
-#wallet-list-header {
-	margin-bottom: 1.125em;
-	font-size: 1.125em;
-	font-weight: bold;
-}
-
-.wallet,
-#add-wallet {
-	padding: 1.25em 0.5em;
-	border-top: 1px solid #e7e8ea;
-	display: flex;
-	align-items: center;
-}
-
-#add-wallet-icon {
-	display: flex;
-	height: 2em;
-	width: 2em;
-	align-items: center;
-	justify-content: center;
-	border-radius: 1em;
-	background: #d6d6d6;
-}
-
-.wallet-details,
-#add-wallet-text {
-	margin-left: 0.75em;
-}
-
-.wallet-address {
-	font-size: 0.875em;
-	color: #666;
-}
-
-.wallet-value {
-	font-size: 1.125em;
-	font-weight: bold;
-}
-
-#add-wallet-icon > img {
-	height: 1em;
-	width: 1em;
-}
-
-#add-wallet-subtitle {
-	font-size: 0.875em;
-	color: #666;
 }
 
 #asset-section {
