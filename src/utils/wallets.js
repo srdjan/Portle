8c9ba1831eeb1fed09b6fe1c88119f31959e1c45
Timur Badretdinov <destinerx@gmail.com>
2020-01-16 15:13:11 +0300

    Add Wallets util class

---
+++ src/utils/wallets.js
@@ -0,0 +1,80 @@
+class Wallets {
+	static getAssets(wallets) {
+		const assets = [];
+		if (wallets.length == 0) {
+			return assets;
+		}
+		for (const walletIndex in wallets) {
+			const walletId = parseInt(walletIndex);
+			const wallet = wallets[walletId];
+			for (const assetId in wallet.assets) {
+				const balance = wallet.assets[assetId];
+				if (balance != '0') {
+					const asset = {
+						walletId,
+						assetId,
+						balance,
+					};
+					assets.push(asset);
+				}
+			}
+		}
+		return assets;
+	}
+
+	static getDeposits(wallets) {
+		const deposits = [];
+		if (wallets.length == 0) {
+			return deposits;
+		}
+		for (const walletIndex in wallets) {
+			const walletId = parseInt(walletIndex);
+			const wallet = wallets[walletId];
+			for (const platformId in wallet.deposits) {
+				const platformBalance = wallet.deposits[platformId];
+				for (const assetId in platformBalance) {
+					const balance = platformBalance[assetId];
+					if (balance != '0') {
+						const deposit = {
+							walletId,
+							platformId,
+							assetId,
+							balance,
+						};
+						deposits.push(deposit);
+					}
+				}
+			}
+		}
+		return deposits;
+	}
+
+	static getInvestments(wallets) {
+		const investments = [];
+		if (wallets.length == 0) {
+			return investments;
+		}
+		for (const walletIndex in wallets) {
+			const walletId = parseInt(walletIndex);
+			const wallet = wallets[walletId];
+			for (const platformId in wallet.investments) {
+				const platformBalance = wallet.investments[platformId];
+				for (const investmentId in platformBalance) {
+					const balance = platformBalance[investmentId];
+					if (balance != '0') {
+						const deposit = {
+							walletId,
+							platformId,
+							investmentId,
+							balance,
+						};
+						investments.push(deposit);
+					}
+				}
+			}
+		}
+		return investments;
+	}
+}
+
+export default Wallets;
