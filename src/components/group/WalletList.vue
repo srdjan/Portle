1e46164f8e0a8d775f1e088375ddac22e57358aa
Timur Badretdinov <destinerx@gmail.com>
2020-01-16 15:13:29 +0300

    Use Wallets in Portfolio and WalletList

--- src/components/group/WalletList.vue
+++ src/components/group/WalletList.vue
@@ -49,6 +49,7 @@ import WalletIcon from '../WalletIcon.vue';
 
 import Balance from '../../utils/balance.js';
 import Formatter from '../../utils/formatter.js';
+import Wallets from '../../utils/wallets.js';
 
 export default {
 	components: {
@@ -70,78 +71,13 @@ export default {
 	},
 	computed: {
 		assets() {
-			const assets = [];
-			if (this.wallets.length == 0) {
-				return assets;
-			}
-			for (const walletIndex in this.wallets) {
-				const walletId = parseInt(walletIndex);
-				const wallet = this.wallets[walletId];
-				for (const assetId in wallet.assets) {
-					const balance = wallet.assets[assetId];
-					if (balance != '0') {
-						const asset = {
-							walletId,
-							assetId,
-							balance,
-						};
-						assets.push(asset);
-					}
-				}
-			}
-			return assets;
+			return Wallets.getAssets(this.wallets);			
 		},
 		deposits() {
-			const deposits = [];
-			if (this.wallets.length == 0) {
-				return deposits;
-			}
-			for (const walletIndex in this.wallets) {
-				const walletId = parseInt(walletIndex);
-				const wallet = this.wallets[walletId];
-				for (const platformId in wallet.deposits) {
-					const platformBalance = wallet.deposits[platformId];
-					for (const assetId in platformBalance) {
-						const balance = platformBalance[assetId];
-						if (balance != '0') {
-							const deposit = {
-								walletId,
-								platformId,
-								assetId,
-								balance,
-							};
-							deposits.push(deposit);
-						}
-					}
-				}
-			}
-			return deposits;
+			return Wallets.getDeposits(this.wallets);			
 		},
 		investments() {
-			const investments = [];
-			if (this.wallets.length == 0) {
-				return investments;
-			}
-			for (const walletIndex in this.wallets) {
-				const walletId = parseInt(walletIndex);
-				const wallet = this.wallets[walletId];
-				for (const platformId in wallet.investments) {
-					const platformBalance = wallet.investments[platformId];
-					for (const investmentId in platformBalance) {
-						const balance = platformBalance[investmentId];
-						if (balance != '0') {
-							const deposit = {
-								walletId,
-								platformId,
-								investmentId,
-								balance,
-							};
-							investments.push(deposit);
-						}
-					}
-				}
-			}
-			return investments;
+			return Wallets.getInvestments(this.wallets);			
 		},
 		totalBalance() {
 			if (this.wallets.length == 0) {
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -74,6 +74,7 @@ import Converter from '../utils/converter.js';
 import Formatter from '../utils/formatter.js';
 import Loader from '../utils/loader.js';
 import Storage from '../utils/storage.js';
+import Wallets from '../utils/wallets.js';
 
 import addresses from '../data/addresses.json';
 
@@ -116,78 +117,13 @@ export default {
 	},
 	computed: {
 		assets() {
-			const assets = [];
-			if (this.wallets.length == 0) {
-				return assets;
-			}
-			for (const walletIndex in this.wallets) {
-				const walletId = parseInt(walletIndex);
-				const wallet = this.wallets[walletId];
-				for (const assetId in wallet.assets) {
-					const balance = wallet.assets[assetId];
-					if (balance != '0') {
-						const asset = {
-							walletId,
-							assetId,
-							balance,
-						};
-						assets.push(asset);
-					}
-				}
-			}
-			return assets;
+			return Wallets.getAssets(this.wallets);
 		},
 		deposits() {
-			const deposits = [];
-			if (this.wallets.length == 0) {
-				return deposits;
-			}
-			for (const walletIndex in this.wallets) {
-				const walletId = parseInt(walletIndex);
-				const wallet = this.wallets[walletId];
-				for (const platformId in wallet.deposits) {
-					const platformBalance = wallet.deposits[platformId];
-					for (const assetId in platformBalance) {
-						const balance = platformBalance[assetId];
-						if (balance != '0') {
-							const deposit = {
-								walletId,
-								platformId,
-								assetId,
-								balance,
-							};
-							deposits.push(deposit);
-						}
-					}
-				}
-			}
-			return deposits;
+			return Wallets.getDeposits(this.wallets);
 		},
 		investments() {
-			const investments = [];
-			if (this.wallets.length == 0) {
-				return investments;
-			}
-			for (const walletIndex in this.wallets) {
-				const walletId = parseInt(walletIndex);
-				const wallet = this.wallets[walletId];
-				for (const platformId in wallet.investments) {
-					const platformBalance = wallet.investments[platformId];
-					for (const investmentId in platformBalance) {
-						const balance = platformBalance[investmentId];
-						if (balance != '0') {
-							const deposit = {
-								walletId,
-								platformId,
-								investmentId,
-								balance,
-							};
-							investments.push(deposit);
-						}
-					}
-				}
-			}
-			return investments;
+			return Wallets.getInvestments(this.wallets);
 		},
 		assetBalance() {
 			const balance = Balance.getAssets(this.assets, this.prices);
