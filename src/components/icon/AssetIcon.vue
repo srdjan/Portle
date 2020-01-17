c4b43dcb6c755507ed4e0173e4ef3654e3bc1d51
Timur Badretdinov <destinerx@gmail.com>
2020-01-17 18:26:35 +0300

    Add AssetIcon component

---
+++ src/components/icon/AssetIcon.vue
@@ -0,0 +1,21 @@
+<template>
+	<img :src="icon">
+</template>
+
+<script>
+import AssetLoader from '../../utils/assetLoader.js';
+
+export default {
+	props: {
+		id: {
+			type: String,
+			default: '',
+		},
+	},
+	computed: {
+		icon() {
+			return AssetLoader.loadAssetLogo(this.id);
+		},
+	}
+};
+</script>
