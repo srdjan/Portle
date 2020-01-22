15f2da170018c66f17e94e9c522c1e3da6ece0cb
Timur Badretdinov <destinerx@gmail.com>
2020-01-22 16:20:31 +0300

    Add UniswapIcon component

---
+++ src/components/icon/UniswapIcon.vue
@@ -0,0 +1,28 @@
+<template>
+	<AssetIcon
+		v-if="assetId"
+		:asset-id="assetId"
+	/>
+</template>
+
+<script>
+import AssetIcon from './AssetIcon.vue';
+
+export default {
+	components: {
+		AssetIcon,
+	},
+	props: {
+		poolId: {
+			type: String,
+			default: '',
+		},
+	},
+	computed: {
+		assetId() {
+			const items = this.poolId.split('_');
+			return items[0];
+		},
+	}
+};
+</script>
