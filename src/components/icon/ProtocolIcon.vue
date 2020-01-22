c074a9e59925136227ae1d61c06e6adcb94a0fd7
Timur Badretdinov <destinerx@gmail.com>
2020-01-22 16:41:58 +0300

    Add ProtocolIcon components

---
+++ src/components/icon/ProtocolIcon.vue
@@ -0,0 +1,24 @@
+<template>
+	<img
+		v-if="icon"
+		:src="icon"
+	>
+</template>
+
+<script>
+import AssetLoader from '../../utils/assetLoader.js';
+
+export default {
+	props: {
+		protocolId: {
+			type: String,
+			default: '',
+		},
+	},
+	computed: {
+		icon() {
+			return AssetLoader.loadProtocolLogo(this.protocolId);
+		},
+	}
+};
+</script>
