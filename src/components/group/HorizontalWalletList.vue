4197f618bdafd63d91ac05dede731691b770eb25
Timur Badretdinov <destinerx@gmail.com>
2020-01-23 20:40:36 +0300

    Add HorizontalWalletList component

---
+++ src/components/group/HorizontalWalletList.vue
@@ -0,0 +1,275 @@
+<template>
+	<div>
+		<div
+			id="total-value-wrapper"
+			:class="{ 'total-value-wrapper-selected': selectedWallet == -1 }"
+			@click="openPortfolio()"
+		>
+			<div id="total-value-label">
+				Net worth:
+			</div>
+			<div id="total-value">
+				{{ formatMoney(totalBalance) }}
+			</div>
+		</div>
+
+		<div id="wallet-list">
+			<div
+				v-for="(wallet, walletId) in wallets"
+				:key="wallet.address"
+				class="wallet"
+				:class="{ 'wallet-selected': walletId == selectedWallet }"
+			>
+				<div class="wallet-data">
+					<WalletIcon
+						:wallet-id="walletId"
+						class="wallet-icon"
+					/>
+					<div
+						class="wallet-details"
+						@click="openWallet(wallet.address)"
+					>
+						<div class="wallet-address">
+							{{ formatWalletAddress(wallet.address) }}
+						</div>
+						<div class="wallet-value">
+							{{ formatMoney(walletBalance(walletId)) }}
+						</div>
+					</div>
+				</div>
+				<div class="wallet-actions">
+					<img
+						:src="crossIcon"
+						class="remove-wallet-icon"
+						@click="removeWallet(wallet)"
+					>
+				</div>
+			</div>
+		</div>
+
+		<div
+			id="add-wallet"
+			@click="openNewWallet()"
+		>
+			<img
+				id="add-wallet-icon"
+				:src="plusIcon"
+			>
+			<div id="add-wallet-text">
+				Wallet
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import crossIcon from '../../../public/img/cross.svg';
+import plusIcon from '../../../public/img/plus.svg';
+
+import WalletIcon from '../icon/WalletIcon.vue';
+
+import Balance from '../../utils/balance.js';
+import Formatter from '../../utils/formatter.js';
+import Storage from '../../utils/storage.js';
+import Wallets from '../../utils/wallets.js';
+
+export default {
+	components: {
+		WalletIcon,
+	},
+	props: {
+		wallets: {
+			type: Array,
+			default: () => [],
+		},
+		selectedWallet: {
+			type: Number,
+			default: -1,
+		},
+		components: {
+			type: Object,
+			default: () => {},
+		},
+		prices: {
+			type: Object,
+			default: () => {},
+		},
+	},
+	computed: {
+		assets() {
+			return Wallets.getAssets(this.wallets);			
+		},
+		deposits() {
+			return Wallets.getDeposits(this.wallets);			
+		},
+		investments() {
+			return Wallets.getInvestments(this.wallets);			
+		},
+		totalBalance() {
+			if (this.wallets.length == 0) {
+				return '0';
+			}
+			const balance = Balance.getTotal(
+				this.assets,
+				this.deposits,
+				this.investments,
+				this.components,
+				this.prices
+			);
+			return balance.toString();
+		},
+		crossIcon() {
+			return crossIcon;
+		},
+		plusIcon() {
+			return plusIcon;
+		},
+	},
+	methods: {
+		walletBalance(walletId) {
+			const walletAssets = this.assets
+				.filter(asset => asset.walletId == walletId);
+			const walletDeposits = this.deposits
+				.filter(deposit => deposit.walletId == walletId);
+			const walletInvestments = this.investments
+				.filter(investment => investment.walletId == walletId);
+			const walletBalance = Balance.getTotal(
+				walletAssets,
+				walletDeposits,
+				walletInvestments,
+				this.components,
+				this.prices
+			);
+			return walletBalance.toString();
+		},
+		formatMoney(moneyString) {
+			return Formatter.formatMoney(moneyString);
+		},
+		formatWalletAddress(address) {
+			return Formatter.formatAddress(address);
+		},
+		removeWallet(wallet) {
+			Storage.removeWallet(wallet);
+			this.$router.go();
+		},
+		openPortfolio() {
+			const path = '/';
+			this.$router.push(path);
+		},
+		openWallet(address) {
+			const path = `/wallet/${address}`;
+			this.$router.push(path);
+		},
+		openNewWallet() {
+			const path = '/wallet/new';
+			this.$router.push(path);
+		},
+	},
+};
+</script>
+
+<style scoped>
+#total-value-wrapper {
+	display: flex;
+	flex-direction: column;
+	font-size: 1.125em;
+	cursor: pointer;
+}
+
+#total-value-label {
+	width: 9em;
+	font-size: 0.625rem;
+	color: var(--inverted-secondary-text-color);
+}
+
+.total-value-wrapper-selected > #total-value-label {
+	color: var(--inverted-primary-text-color);
+}
+
+.total-value-wrapper-selected > #total-value {
+	font-weight: bold;
+}
+
+#wallet-list {
+	display: flex;
+	margin-left: 1em;
+}
+
+.wallet {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	height: 2.25em;
+	margin-left: 0.5em;
+}
+
+.wallet-details {
+	margin-left: 0.25em;
+	height: 2.25em;
+	cursor: pointer;
+}
+
+.wallet:hover > .wallet-actions {
+	display: initial;
+}
+
+.remove-wallet-icon {
+	height: 1.25em;
+	width: 1.25em;
+	opacity: 0.5;
+	filter: invert();
+}
+
+.wallet-selected .wallet-actions > img {
+	filter: invert(100%);
+}
+
+.wallet-data {
+	display: flex;
+	align-items: center;
+}
+
+.wallet-icon {
+	height: 1em;
+	width: 1em;
+	opacity: 0.75;
+}
+
+.wallet-address {
+	width: 9em;
+	font-size: 0.625em;
+	color: var(--inverted-secondary-text-color);
+}
+
+.wallet-value {
+	font-size: 1.125em;
+}
+
+.wallet-selected .wallet-address,
+.wallet-selected .wallet-value {
+	color: var(--inverted-primary-text-color);
+	font-weight: bold;
+}
+
+#add-wallet {
+	display: flex;
+	align-items: center;
+	padding: 0 0.5em;
+	margin-left: 1em;
+	border: 1px solid var(--inverted-primary-text-color);
+	cursor: pointer;
+}
+
+#add-wallet-icon {
+	height: 1.125em;
+	width: 1.125em;
+	opacity: 1;
+	filter: invert();
+}
+
+#add-wallet-text {
+	width: 2.75em;
+	margin-left: 0.5em;
+	font-size: 0.75em;
+}
+</style>
